07fa69ae51783c83994f2965a4fb81b446a16568
Duncan Keall <duncan@duncankeall.com>
2015-01-24 11:33:45 +1300

    Also protect signouts from CSRF attacks.

--- signout_handler.go
+++ signout_handler.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/subtle"
+	"fmt"
 	"net/http"
 )
 
@@ -9,13 +11,28 @@ type SignoutHandler HandlerWithDBConnection
 func (hd *SignoutHandler) ServeHTTP(resp http.ResponseWriter, request *http.Request) {
 	db := hd.db
 
+	session, err := sessionFromRequest(request, db)
+	if err == ErrInvalidSession {
+		fmt.Println(err)
+		clearSessionCookie(resp)
+	}
+	signedIn := err == nil
+
+	if !signedIn {
+		fmt.Println("not signed in")
+		http.Error(resp, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	if subtle.ConstantTimeCompare([]byte(request.PostFormValue("csrf_token")), []byte(session.csrfToken)) != 1 {
+		fmt.Println("invalid csrf token")
+		http.Error(resp, "Invalid CSRF token", http.StatusBadRequest)
+		return
+	}
+
 	clearSessionCookie(resp)
 
-	cookie, err := request.Cookie("session")
-	if err == nil {
-		sessionToken := cookie.Value
-		db.Exec("delete from sessions where session_token = ?", sessionToken)
-	}
+	db.Exec("delete from sessions where session_id = ?", session.sessionId)
 
 	http.Redirect(resp, request, "/", http.StatusSeeOther)
 }
